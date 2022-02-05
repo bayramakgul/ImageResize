a8ed1672dad68d9a41d3d5202be3a670697553ef
bayramakgul <bayramakgul@gmail.com>
2022-02-06 02:49:05 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ImageResize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.nmWidth = new System.Windows.Forms.NumericUpDown();
+            this.nmHeight = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblState = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblProgress = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.nmWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmHeight)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(17, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(605, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBrowse.Image = global::ImageResize.Properties.Resources.folder;
+            this.btnBrowse.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBrowse.Location = new System.Drawing.Point(17, 13);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(605, 55);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Resimler Klasörünü Seçiniz:";
+            this.btnBrowse.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // nmWidth
+            // 
+            this.nmWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nmWidth.Location = new System.Drawing.Point(503, 111);
+            this.nmWidth.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nmWidth.Name = "nmWidth";
+            this.nmWidth.Size = new System.Drawing.Size(120, 26);
+            this.nmWidth.TabIndex = 7;
+            this.nmWidth.Value = new decimal(new int[] {
+            224,
+            0,
+            0,
+            0});
+            // 
+            // nmHeight
+            // 
+            this.nmHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nmHeight.Location = new System.Drawing.Point(503, 143);
+            this.nmHeight.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nmHeight.Name = "nmHeight";
+            this.nmHeight.Size = new System.Drawing.Size(120, 26);
+            this.nmHeight.TabIndex = 8;
+            this.nmHeight.Value = new decimal(new int[] {
+            224,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(397, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Widht (Pixel):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(391, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Height (Pixel):";
+            // 
+            // btnResize
+            // 
+            this.btnResize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnResize.Location = new System.Drawing.Point(395, 182);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(228, 56);
+            this.btnResize.TabIndex = 11;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblState,
+            this.progressBar,
+            this.lblProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 268);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(635, 24);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblState
+            // 
+            this.lblState.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(458, 19);
+            this.lblState.Spring = true;
+            this.lblState.Text = "...";
+            this.lblState.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // progressBar
+            // 
+            this.progressBar.AutoSize = false;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(200, 18);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = false;
+            this.lblProgress.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(122, 19);
+            this.lblProgress.Text = ".../ ...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 60);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Kaydetme \r\nformatı\r\n seçiniz:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(105, 111);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(133, 144);
+            this.listBox1.TabIndex = 13;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 292);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nmHeight);
+            this.Controls.Add(this.nmWidth);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Image Resize";
+            ((System.ComponentModel.ISupportInitialize)(this.nmWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmHeight)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.NumericUpDown nmWidth;
+        private System.Windows.Forms.NumericUpDown nmHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblState;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ToolStripStatusLabel lblProgress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageResize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add(ImageFormat.MemoryBmp);
+            listBox1.Items.Add(ImageFormat.Bmp);
+            listBox1.Items.Add(ImageFormat.Jpeg);
+            listBox1.Items.Add(ImageFormat.Png);
+
+            listBox1.SelectedIndex = 2;
+
+        }
+
+
+        List<FileInfo> images;
+        string savepath = "";
+        ImageFormat SaveFormat;
+        string ext;
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+                textBox1.Text = fd.SelectedPath;
+            else
+                return;
+
+            savepath = textBox1.Text + $"_{nmWidth.Value}x{nmHeight.Value}\\";
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            var bmp = dir.GetFiles("*.bmp");
+            var jpg = dir.GetFiles("*.jpg");
+            var jpeg = dir.GetFiles("*.jpeg");
+            var png = dir.GetFiles("*.png");
+
+            images = new List<FileInfo>();
+            images.AddRange(bmp);
+            images.AddRange(jpg);
+            images.AddRange(jpeg);
+            images.AddRange(png);
+
+            lblProgress.Text = $"0 / {images.Count}";
+            progressBar.Maximum = images.Count;
+            progressBar.Value = 0;
+
+            lblState.Text = "Hazır.";
+
+        }
+
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            lblState.Text = "İşlem Başlatılıyor...";
+            SaveFormat = listBox1.SelectedItem as ImageFormat;
+
+            if (SaveFormat == ImageFormat.Bmp || SaveFormat == ImageFormat.MemoryBmp)
+                ext = ".bmp";
+            else if (SaveFormat == ImageFormat.Jpeg)
+                ext = ".jpeg";
+            else if (SaveFormat == ImageFormat.Png)
+                ext = ".png";
+
+            btnResize.Enabled = false;
+            backgroundWorker.RunWorkerAsync();
+
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(savepath);
+            if (!dir.Exists)
+                dir.Create();
+
+            Size size = new Size((int)nmWidth.Value, (int)nmHeight.Value);
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                FileInfo file = images[i];
+                Image img = Image.FromFile(file.FullName);
+
+                img = ResizeImage(img, size);
+
+                string name = file.Name.Substring(0, file.Name.LastIndexOf("."));
+
+                img.Save($"{savepath}{name}{ext}", SaveFormat);
+
+                backgroundWorker.ReportProgress(i, file.FullName);
+            }
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblProgress.Text = $"{e.ProgressPercentage + 1} / {images.Count}";
+            lblState.Text = e.UserState.ToString();
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblState.Text = "Bitti.";
+            btnResize.Enabled = true;
+            progressBar.Value = 0;
+
+        }
+
+        public static Image ResizeImage(Image img, Size newSize)
+        {
+            if (img.Width > newSize.Width || img.Height > newSize.Height)
+            {
+                return img.GetThumbnailImage(newSize.Width, newSize.Height, () => false, IntPtr.Zero);
+            }
+            return img;
+        }
+
+    }
+}
